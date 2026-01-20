3e8fc06f87c51dc1318014ab283cd97404eb4324
mj-codices <mjwhite.dev@gmail.com>
2026-01-20 15:30:26 -0800

    Edited hero image layout; Moved hero heading to own component; added fade in effect on mount to hero heading; Created FadeinWrapper, added wrapper to Hero content

--- src/app/components/hero/Hero.tsx
+++ src/app/components/hero/Hero.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import HeroHeading from "./HeroHeading";
+import FadeInWrapper from "../FadeInWrapper";
 
 export default function Hero() {
   return (
@@ -8,35 +10,34 @@ export default function Hero() {
     >
       {/* Left Side - Text */}
       <div className="flex-1 text-center md:text-left pl-35">
-        <div className="tracking-[-.2rem]">
-          <h1 className="text-7xl font-bold mt-10 mb-6 uppercase leading-[3.9rem]">
-            <span className="gradient-text">Full-stack</span>
-            <br />
-            <span className="pl-5 text-white">Developer</span>
-          </h1>
-        </div>
-        <p className="text-lg mb-10 max-w-lg leading-[2.3rem] tracking-[.06rem]">
-          Hello! I’m <span className="text-white">Michael White</span> (most
-          people call me Julian). I build thoughtful, scalable, and
-          production-ready web apps.
-        </p>
-        <button className="ml-1 px-5 py-5 bg-[var(--color-accent)] text-[var(--color-secondary)] rounded font-semibold text-xl tracking-wider cursor-pointer button button--calypso">
-          <span>LET'S CONNECT</span>
-          <span>SEND A MESSAGE</span>
-        </button>
+        <HeroHeading />
+        <FadeInWrapper delay={1500} duration={500}>
+          <p className="text-lg mb-10 max-w-lg leading-[2.3rem] tracking-[.06rem]">
+            Hello! I’m <span className="text-white">Michael White</span> (most
+            people call me Julian). I build thoughtful, scalable, and
+            production-ready web apps.
+          </p>
+        </FadeInWrapper>
+
+        <FadeInWrapper delay={1500} duration={500}>
+          <button className="ml-1 px-5 py-5 bg-[var(--color-accent)] text-[var(--color-secondary)] rounded font-semibold text-xl tracking-wider cursor-pointer button button--calypso">
+            <span>LET'S CONNECT</span>
+            <span>SEND A MESSAGE</span>
+          </button>
+        </FadeInWrapper>
       </div>
 
       {/* Right Side - Image */}
       {/* Decorative Hero Image (out of document flow) */}
       <div
         className="absolute right-0 top-1/2 -translate-y-75 pointer-events-none
-      translate-x-72
+      translate-x-68
 "
       >
         <Image
           src="/image_fake.svg"
           alt="Michael White"
-          width={1300}
+          width={1250}
           height={1200}
           className="opacity-90"
         />
---
+++ src/app/components/hero/HeroHeading.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function HeroHeading() {
+    const [animate, setAnimate] = useState(false);
+
+    useEffect(() => {
+        requestAnimationFrame(() => setAnimate(true));
+    }, []);
+
+  const line1 = "Full-stack";
+  const line2 = "Developer";
+  
+  const renderLine = (
+    text: string, 
+    lineDelay = 0,
+    lineClass = ""
+  ) => (
+    <span className={`block ${lineClass}`}>
+        <span className="inline-block">
+    <span className={`text-anim ${animate ? "animate" : ""}`}>
+
+            {text.split("").map((char, i) => (
+                <span
+                    key={i}
+                    className="inline-block"
+                    style={{ transitionDelay: `${lineDelay + i * 55}ms`,
+                }}
+                >
+                    {char === " " ? "\u00A0" : char}                    
+                </span>
+            ))}
+            </span>
+        </span>
+    </span>
+  );
+
+  return (
+    <h1 className="text-7xl font-bold mt-10 mb-6 uppercase leading-[3.9rem] tracking-[-.2rem]">
+        {renderLine(line1, 0, "gradient-text")}
+        {renderLine(line2, 1000, "text-white pl-4")}
+    </h1>
+  );
+}
