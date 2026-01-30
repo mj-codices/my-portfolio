2b3cc76113eb88209a791772cfbdb1a198d613ca
mj-codices <mjwhite.dev@gmail.com>
2026-01-30 15:40:28 -0800

    Added icon to menu - commented out till further notice;  Added linear gradient to hamburger icon;

--- src/app/components/nav/Menu.tsx
+++ src/app/components/nav/Menu.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface MenuProps {
   open: boolean;
   onToggle: () => void;
@@ -9,6 +11,18 @@ export default function Menu({ open, onToggle }: MenuProps) {
       className=" fixed
         z-60"
     >
+      <div className="logo-container w-[80px] h-[80px] relative">
+      {/* <Image
+        src={"/logo.svg"}
+        
+        width={70}
+        height={70}
+        alt="my logo"
+        className="ml-15 mt-10 z-20 top-0 left-0 absolute w-full h-full transition-opacity duration-500 hover:opacity-0"
+        >
+      </Image> */}
+      </div>
+      
       <label
         className="
         menu menu--1 cross
@@ -34,6 +48,20 @@ export default function Menu({ open, onToggle }: MenuProps) {
           height="105"
           xmlns="http://www.w3.org/2000/svg"
         >
+            <defs>
+    <linearGradient
+      id="hamburgerGradient"
+      gradientUnits="userSpaceOnUse"
+      x1="0"
+      y1="0"
+      x2="100"
+      y2="0"
+    >
+      <stop offset="20%" stopColor="#191919" />
+      <stop offset="100%" stopColor="#706f6f" />
+    </linearGradient>
+  </defs>
+
           {/* Menu lines */}
 
           <path className="line--1" d="M0 40h62c13 0 6 28-4 18L35 35" />
